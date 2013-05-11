ada334ee7edfb7b35eecb7fe8e12e8487f68f295
Alexander Jolovic <ajolovic@me.com>
2013-05-11 14:05:07 -0700

    You can now update a project.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
 
+
   def index
    @projects = Project.all 
   end
@@ -23,9 +24,28 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash[:alert] = "Project has not been updated."
+      render :action => "edit"
+    end
+  end
+
 
 private
   def project_params
     params.require(:project).permit(:name, :description)
+  end
+
+  def find_project
+    @project = Project.find(params[:id])
   end
 end
