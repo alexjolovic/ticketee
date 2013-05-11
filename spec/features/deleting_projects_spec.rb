bbb7b22961fca661fab7d28b2189bd09b57a30f0
Alexander Jolovic <ajolovic@me.com>
2013-05-11 14:10:34 -0700

    Implement delete functionality for projects

---
+++ spec/features/deleting_projects_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+feature "Deleting projects" do
+  
+  scenario "Deleting a project" do
+    
+    FactoryGirl.create(:project, name: "TextMate 2")
+    
+    visit root_path
+    
+    click_link "TextMate 2"
+    
+    click_link "Delete Project"
+    
+    expect(page).to have_content("Project has been destroyed.")
+
+    visit root_path
+    
+    expect(page).to have_no_content("TextMate 2")
+  
+  end
+
+end
